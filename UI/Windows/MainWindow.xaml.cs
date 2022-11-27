aa7f7956dccede3580e29208452016ffd2167463
home <home@MYCOMPIK>
2022-11-27 20:27:00 +0300

    Добавьте файлы проекта.

---
+++ UI/Windows/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace RestaurantApp
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            mainFrame.Content = new UI.Pages.MenuPage();
+        }
+
+        private void ButtonMenu_Click(object sender, RoutedEventArgs e)
+        {
+            mainFrame.Content = new UI.Pages.MenuPage();
+        }
+
+        private void ButtonAboutRestaurant_Click(object sender, RoutedEventArgs e)
+        {
+            mainFrame.Content = new UI.Pages.MainPage();
+        }
+    }
+}
